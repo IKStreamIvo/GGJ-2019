f851f32476b7ee77d2ff1ccfd9edf40b8417bb94
Kris Galama <krisgalama@gmail.com>
2019-01-25 18:53:58 +0100

    facing direction bool

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -8,12 +8,13 @@ public class PlayerInput : MonoBehaviour {
 
 	public float moveSpeed;
 
+	[SerializeField] private bool m_FacingRight;
+
 	private void Start() {
 		if(r2d == null) {
 			r2d = GetComponent<Rigidbody2D>();
 		}
 	}
-
 
 	// a jumps
 	// b crouch
@@ -22,8 +23,10 @@ public class PlayerInput : MonoBehaviour {
 	void Update() {
 		if(Input.GetAxis("Horizontal") > 0f) {
 			r2d.AddForce(Vector2.right * moveSpeed);
+			m_FacingRight = true;
 		} else if (Input.GetAxis("Horizontal") < 0f){
 			r2d.AddForce(Vector2.left * moveSpeed);
+			m_FacingRight = false;
 		} else {
 			r2d.velocity = Vector2.zero;
 		}
