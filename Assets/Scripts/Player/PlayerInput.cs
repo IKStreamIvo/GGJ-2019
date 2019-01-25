3a974da6fbc7fc51062d2a330fbd5dc87c356edf
Kris Galama <krisgalama@gmail.com>
2019-01-25 18:31:20 +0100

    basic player movement

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerInput : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+	Rigidbody2D r2d;
+
+	public float moveSpeed;
+
+	private void Start() {
+		if(r2d == null) {
+			r2d = GetComponent<Rigidbody2D>();
+		}
+	}
+
+	// a jumps
+	// b crouch
+	// left toggle for axis.
+	// Update is called once per frame
+	void Update() {
+		if(Input.GetAxis("Horizontal") > 0f) {
+			r2d.AddForce(Vector2.right * moveSpeed);
+		} else if (Input.GetAxis("Horizontal") < 0f){
+			r2d.AddForce(Vector2.left * moveSpeed);
+		} else {
+			r2d.velocity = Vector2.zero;
+		}
     }
 }
