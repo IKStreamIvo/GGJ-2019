811d4128be934ee1a599f7ab584eb3898b4f7365
Kris Galama <krisgalama@gmail.com>
2019-01-26 11:22:12 +0100

    adding a gamecontroller script

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	[SerializeField] private static GameController gCont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		gCont = this;
+    }
+
+	void LoadNewScene(int sceneToLoad) {
+		SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+	}
+
+	void LoadNewScene(string sceneToLoad) {
+		SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+	}
+}
